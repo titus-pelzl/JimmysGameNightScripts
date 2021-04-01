6ebc768f1afb2050636d5edaa844cad6aa4b2df1
titus-pelzl <52253305+titus-pelzl@users.noreply.github.com>
2021-04-01 13:02:46 +0000

    Add files via upload
    Added scripts for Scenemanagement and Spawning

---
+++ Scene/SceneSpawnPoint.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneSpawnPoint : MonoBehaviour
+{
+    public List<Transform> SpawnPoints;
+
+    public GameObject Sword;
+
+    public void Start()
+    {
+        foreach (Transform Child in transform)
+        {
+            SpawnPoints.Add(Child);
+
+        }
+
+        GameObject Player = Resources.Load<GameObject>("Prefabs/Plyr");
+        GameObject ply =Instantiate(Player,SpawnPoints[SceneSwitchInfo.NextSpawnPoint].position,new Quaternion(0f,0f,0f,0f));
+        Sword = ply.transform.GetChild(0).GetChild(1).gameObject;
+        Cursor.SetCursor(Resources.Load<Texture2D>("Cursor/cursor.idle"),Input.mousePosition,CursorMode.ForceSoftware);
+    }
+}
---
+++ Scene/SceneSwitchTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneSwitchTrigger : MonoBehaviour
+{
+    //Auf Hitbox ziehen
+    public string NextScene;
+    public int NextSpawnPoint;
+    
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Debug.Log("Wechsel in " + NextScene + ", an SpawnPoint " + NextSpawnPoint);
+            SceneSwitchInfo.NextSpawnPoint = NextSpawnPoint;
+            UnityEngine.SceneManagement.SceneManager.LoadScene(NextScene);
+        }
+    }
+
+}
